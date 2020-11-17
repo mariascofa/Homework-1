41f521778cf0cf98342958f2fa79e2728c707d68
Maria Scofa <mariascofa@gmail.com>
2020-11-17 02:59:49 +0200

    First commit!

---
+++ gitprj.py
@@ -0,0 +1,26 @@
+min = int(input("Введите минимальное значение счетчика:"))
+max = int(input("Введите максимальное значение счетчика:"))
+class Lichylnyk :
+    def __init__(self, min=0, max=0):
+        self.min = min
+        self.max = max
+        self.cur=min
+
+
+    def show(self):
+        print("Текущее значения-",self.cur)
+
+    def move(self):
+        if self.cur < self.max:
+            self.cur = self.cur + 1
+        else:
+            self.cur = self.min
+
+
+pt1 = Lichylnyk(min, max)
+for _ in range (max-min):
+    pt1.show()
+    pt1.move()
+
+
+
